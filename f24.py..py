ff4bc773d738a8b8aa4e9a605f95e2dce3095a36
wlauth <42503899+wlauth@users.noreply.github.com>
2018-11-14 12:00:06 -1000

    Add files via upload

---
+++ f24.py..py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 10:49:11 2018
+
+@author: Bill
+"""
+nabirth = dict()
+nabirth[Name] ='11-5-2000' 
+nabirth['Ben'] ='11-31-1998'
+#print(nabirth)
+print(nabirth['Amy'])
+print(nabirth['Ben'])
+#import datetime
+#
+#strdate = '11-05-2018'
+#
+#dtbday = datetime.datetime.strptime(strdate, "%m-%d-%Y").date()
+#
+## check
+#print(dtbday)
+
+#Dictionary written to friendsbdays.json
+
+
+#Please enter a name: Amy
+#Please enter Amy's birthdate: 11-05-2000
+#Please enter a name: Ben
+#Please enter Ben's birthdate: 11-31-1998
+#ERROR: There is a problem with that date !!!
+#Please enter Ben's birtydate: 11-05-1998
+#Please enter a name: Amy
+#Amy's birthday is: datetime.date(2000, 11, 5)
+#Please enter a name: Write
+#Dictionary written to friendsbdays.json
+
+#eng2sp = dict() 
+#print(eng2sp) 
+#
+#eng2sp['one'] = 'uno' 
+#print(eng2sp)
+
+Name =  input('Please enter a name ' )
+print(Name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
